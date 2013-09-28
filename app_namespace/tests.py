670aaa2d9ad5ce35337f0ce818aaab5d0e42824b
Fantomas42 <fantomas42@gmail.com>
2013-09-28 21:06:16 +0200

    Preparing the tests

---
+++ app_namespace/tests.py
@@ -0,0 +1,13 @@
+"""Tests for app_namespace"""
+from django.test import TestCase
+
+#from app_namespace import Loader
+
+
+class LoaderTestCase(TestCase):
+
+    def test_get_template(self):
+        pass
+
+    def test_extend_and_override(self):
+        pass
