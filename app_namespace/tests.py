f7de44f6144245cbc5c18580d53004e273f401f7
Fantomas42 <fantomas42@gmail.com>
2013-09-29 10:32:15 +0200

    Adding a more basic test

--- app_namespace/tests.py
+++ app_namespace/tests.py
@@ -2,13 +2,14 @@
 from django.test import TestCase
 from django.template.base import Context
 from django.template.loaders import app_directories
+from django.template.base import TemplateDoesNotExist
 
 from app_namespace import Loader
 
 
 class LoaderTestCase(TestCase):
 
-    def test_load_template_source(self):
+    def test_load_template(self):
         app_namespace_loader = Loader()
         app_directory_loader = app_directories.Loader()
 
@@ -19,6 +20,22 @@ class LoaderTestCase(TestCase):
         context = Context({})
         self.assertEquals(template_directory.render(context),
                           template_namespace.render(context))
+
+    def test_load_template_source(self):
+        app_namespace_loader = Loader()
+        app_directory_loader = app_directories.Loader()
+
+        template_directory = app_directory_loader.load_template_source(
+            'admin/base.html')
+        template_namespace = app_namespace_loader.load_template_source(
+            'admin:admin/base.html')
+        self.assertEquals(template_directory[0], template_namespace[0])
+        self.assertEquals(template_namespace[1],
+                          'namespace:admin:admin/base.html')
+
+        self.assertRaises(TemplateDoesNotExist,
+                          app_namespace_loader.load_template_source,
+                          'unavailable-template')
 
     def test_extend_and_override(self):
         pass
